b85fe7a02d659de229cdda867d3082c4e9165763
Muhan Li <hanhanmumuqq@163.com>
2020-08-24 12:06:26 +0800

    Finish testing batched scratchpad

--- test/test_batched_fps.py
+++ test/test_batched_fps.py
@@ -11,20 +11,24 @@ import numpy as np
 # 19ms per frame (2 pads), per layer at 1024 * 1024, using 2 threads per pad, float
 # constrained by memory bandwidth
 pad_size = (1024, 1024)
-
+pad_num = 2
 
 if __name__ == "__main__":
-    set_omp_max_threads(2)
-    p = BatchedScratchPad(2)
+    set_omp_max_threads(4)
+    p = BatchedScratchPad(pad_num)
     p.load_brush(get_brushes()[0])
     p.reset_all_pads(*pad_size, 1)
 
     # draw rectangle, only works when dtime is set to max
     points = [Point(0.3, 0.3), Point(0.3, 0.6), Point(0.6, 0.6), Point(0.6, 0.3), Point(0.3, 0.3)]
-    p.draw([0, 1], [0, 0], [0, 0], [Setting(1.0, 0.1, 0.5, 0.5, 0.5, 0.5)] * 2, [points] * 2)
+    p.draw(list(range(pad_num)),
+           [0] * pad_num,
+           [0] * pad_num,
+           [Setting(1.0, 0.1, 0.5, 0.5, 0.5, 0.5)] * pad_num,
+           [points] * pad_num)
 
     begin = time()
-    for i in range(200):
-        p.render([0, 1], np.float32)
+    for i in range(2000):
+        p.render(list(range(pad_num)), np.float32)
     end = time()
-    print((end - begin) / 200)
+    print((end - begin) / 2000)
